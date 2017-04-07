c38682e07ecfc2f5983ac1e1bb4f55b9bb40870a
Aaron Nguyen <aaron3831@gmail.com>
2017-04-07 13:32:41 -0700

    format chosen toppings to have space and comma inbetween strings

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,6 @@ function Pizza (topping1, topping2, topping3, topping4, pizzaSize) {
   this.topping3 = topping3;
   this.topping4 = topping4;
   this.pizzaSize = pizzaSize;
-  this.combo = [];
 }
 // Pizza.prototype.ingredients = function () {
 //   var topping1 = "Cheese";
@@ -16,14 +15,25 @@ function Pizza (topping1, topping2, topping3, topping4, pizzaSize) {
 //   var small = "small";
 //
 // };
+Pizza.prototype.pizzaString = function newPizzaToString() {
+  var return1 = this.topping1 + ", " + this.topping2 + ", " + this.topping3 + ", " + this.topping4 + ", " + this.pizzaSize;
+  return return1;
+};
+
 Pizza.prototype.receipt = function () {
-  var topping1 = { name: "Cheese", price: 2.00 };
-  var topping2 = { name: "Pepperoni", price: 2.00 };
-  var topping3 = { name: "Olives", price: 2.00 };
-  var topping4 = { name: "Artichokes", price: 2.00 };
-  var large = { name: "large", price: 6.00 };
-  var medium = { name: "medium", price: 4.00 };
-  var small = { name: "small", price: 2.00 };
+  var Cheese = 2.00;
+  var Pepperoni = 2.00;
+  var Olives = 2.00;
+  var Artichokes = 2.00;
+  var Large = 6.00;
+  var Medium = 4.00;
+  var Small = 2.00 ;
+  var None = 0;
+
+  var add = topping1 + topping2 + topping3 + topping4 + pizzaSize
+
+  return add;
+  $("#addAll").text(add);
 
   // var totalPrice = { name: "Receipt", price: []}
 };
@@ -36,21 +46,26 @@ $(document).ready(function() {
   event.preventDefault();
 
   var firstTopping = $("#toppingForm1").val();
-  console.log(firstTopping)
   var secondTopping = $("#toppingForm2").val();
-  console.log(secondTopping)
   var thirdTopping = $("#toppingForm3").val();
-  console.log(thirdTopping)
   var fourthTopping = $("#toppingForm4").val();
-  console.log(fourthTopping)
   var pizzaSize = $("#toppingForm5").val();
-  console.log(pizzaSize)
 
 
   var newPizza = new Pizza (firstTopping, secondTopping, thirdTopping, fourthTopping, pizzaSize);
   console.log(newPizza)
 
+  console.log(newPizza.pizzaString())
 
+  $("ul#results").append(newPizza.pizzaString());
+  });
+  $("form#toppingList").click(function(event) {
+  event.preventDefault();
+
+  return newPizza.receipt(newPizza);
+  console.log(newPizza.receipt(newPizza))
+
+  $("p#price").text(newPizza);
 
   });
 });
