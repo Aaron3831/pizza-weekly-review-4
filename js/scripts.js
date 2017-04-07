e8d3ac801b87e25ce7d4b05e07063f6ccd4cdd9d
Aaron Nguyen <aaron3831@gmail.com>
2017-04-07 10:33:14 -0700

    add definition to variables in forms

--- js/scripts.js
+++ js/scripts.js
@@ -3,29 +3,24 @@ function Pizza (topping1, topping2, topping3, topping4, sauce, lms) {
   this.topping2 = topping2;
   this.topping3 = topping3;
   this.topping4 = topping4;
-  this.sauce = sauce;
-  this.large = large;
-  this.medium = medium;
-  this.small = small;
+  this.pizzaSize = pizzaSize;
 }
 Pizza.prototype.ingredients = function () {
   var topping1 = { name: "Cheese", price: 2.00 };
   var topping2 = { name: "Pepperoni", price: 2.00 };
   var topping3 = { name: "Olives", price: 2.00 };
   var topping4 = { name: "Artichokes", price: 2.00 };
-  var sauce = { name: "Tomato Sauce", price: 2.00 };
   var large = { name: "large", price: 6.00 };
   var medium = { name: "medium", price: 4.00 };
   var small = { name: "small", price: 2.00 };
 
-  var pizzaIngredients = { name: "Make me a Pizza", toppings: [cheese, pepperoni, olives, artichokes, tomato sauce, large, medium, small], price: [2.00, 2.00, 2.00, 2.00, 2.00, 6.00, 4.00, 2.00] };
+  var pizzaIngredients = { name: "Make me a Pizza", toppings: [cheese, pepperoni, olives, artichokes, large, medium, small], price: [2.00, 2.00, 2.00, 2.00, 6.00, 4.00, 2.00] };
 };
 Pizza.prototype.receipt = function () {
   var topping1 = { name: "Cheese", price: 2.00 };
   var topping2 = { name: "Pepperoni", price: 2.00 };
   var topping3 = { name: "Olives", price: 2.00 };
   var topping4 = { name: "Artichokes", price: 2.00 };
-  var sauce = { name: "Tomato Sauce", price: 2.00 };
   var large = { name: "large", price: 6.00 };
   var medium = { name: "medium", price: 4.00 };
   var small = { name: "small", price: 2.00 };
@@ -35,8 +30,16 @@ Pizza.prototype.receipt = function () {
 
 // UI
 $(document).ready(function() {
-  $("form#toppingList").sibmit(function(event)
+  $("form#toppingList").submit(function(event)
   event.preventDefault();
+
+  var firstTopping = $("#toppingForm1").val();
+  var secondTopping = $("#toppingForm2").val();
+  var thirdTopping = $("#toppingForm3").val();
+  var fourthTopping = $("#toppingForm4").val();
+  var pizzaSize = $("#toppingForm5").val();
+
+  var newPizza = new Pizza (firstTopping, secondTopping, thirdTopping, fourthTopping, pizzaSize);
 
 
   });
