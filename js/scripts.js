e1bfac048f0bd6caedd54dd478e1916ddcb02a6e
Aaron Nguyen <aaron3831@gmail.com>
2017-04-07 16:36:43 -0700

    refactor script

--- js/scripts.js
+++ js/scripts.js
@@ -6,17 +6,7 @@ function Pizza (topping1, topping2, topping3, topping4, pizzaSize) {
   this.pizzaSize = pizzaSize;
   this.addAll = [];
 
-}
-// Pizza.prototype.ingredients = function () {
-//   var topping1 = "Cheese";
-//   var topping2 = "Pepperoni";
-//   var topping3 = "Olives";
-//   var topping4 = "Artichokes";
-//   var large = "large";
-//   var medium = "medium";
-//   var small = "small";
-//
-// };
+};
 Pizza.prototype.pizzaString = function newPizzaToString() {
   var return1 = this.topping1 + ", " + this.topping2 + ", " + this.topping3 + ", " + this.topping4 + ", " + this.pizzaSize;
   return return1;
@@ -27,7 +17,6 @@ Pizza.prototype.receipt = function () {
   this.topping2 = topping2;
   this.topping3 = topping3;
   this.topping4 = topping4;
-  // this.pizzaSize = pizzaSize;
 
   var topping1 = 2;
   var topping2 = 2;
@@ -35,42 +24,12 @@ Pizza.prototype.receipt = function () {
   var topping4 = 2;
   var pizzaSize = $("#toppingForm5").val();
 
-  // var Small = 2;
-  // var Medium = 4;
-  // var Large = 6;
-  // var pizzaSize = (Small || Medium || Large);
-  //
-  // if (pizzaSize = "Small") {
-  //   pizzaSize = 2
-  // } else if (pizzaSize = "Medium") {
-  //   pizzaSize = 4
-  // } else {
-  //   pizzaSize = 6 }
-
-
-
   var addAll = parseInt(topping1 + topping2 + topping3 + topping4 + parseInt(pizzaSize));
 
   console.log(addAll)
-    // if (pizzaSize = "Small") {
-    //   return addAll + 2
-    // }
-    // else if (pizzaSize = "Medium") {
-    //   return addAll + 4
-    // }
-    // else {
-    //   return addAll + 6 }
-
-
-
 
   $("#addAll").text("$" + addAll)
-
-  // this.addAll = parseInt(topping1 + topping2 + topping3 + topping4)
-
   return addAll;
-
-  // var totalPrice = { name: "Receipt", price: []}
 };
 
 
