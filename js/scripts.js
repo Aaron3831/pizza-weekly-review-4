40d612f6a13f8bb362c9be621ff9a16b4e2bfce5
Aaron Nguyen <aaron3831@gmail.com>
2017-04-07 08:41:49 -0700

    add constructor for pizza

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+function Pizza (topping1, topping2, topping3, topping4, sauce, lms) {
+  this.topping1 = topping1;
+  this.topping2 = topping2;
+  this.topping3 = topping3;
+  this.topping4 = topping4;
+  this.sauce = sauce;
+  this.large = large;
+  this.medium = medium;
+  this.small = small;
+}
+Pizza.prototype.ingredients = function () {
+  var topping1 = { name: "Cheese", price: 2.00 };
+  var topping2 = { name: "Pepperoni", price: 2.00 };
+  var topping3 = { name: "Olives", price: 2.00 };
+  var topping4 = { name: "Artichokes", price: 2.00 };
+  var sauce = { name: "Tomato Sauce", price: 2.00 };
+  var large = { name: "large", price: 6.00 };
+  var medium = { name: "medium", price: 4.00 };
+  var small = { name: "small", price: 2.00 };
+
+  var pizzaIngredients = { name: "Make me a Pizza", toppings: [cheese, pepperoni, olives, artichokes, tomato sauce, large, medium, small] };
+};
+Pizza.prototype.receipt = function () {
+  
+};
+
+// UI
+$(document).ready(function(event) {
+  event.preventDefault();
+
+  $("")
+});
